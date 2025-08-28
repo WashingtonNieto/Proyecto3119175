d2f17077ebd14b443e4c26c9bf1fd12fd3447bc7
Washington Nieto Arce <washington_nieto@hotmail.com>
2025-08-27 20:00:15 -0500

    creacion bdatos productos y categorias

--- database/database.sql
+++ database/database.sql
@@ -15,3 +15,24 @@ CREATE TABLE IF NOT EXISTS t_usuarios(
 )ENGINE=InnoDb;
 
 INSERT INTO t_usuarios VALUES(NULL, 'admin', 'admin','admin@admin.com','1234','admin','imagen');
+
+CREATE TABLE IF NOT EXISTS t_categorias(
+    id_categoria        int(11) auto_increment  NOT NULL,
+    nombre_categoria    varchar(100)            NOT NULL,
+    CONSTRAINT pk_categoria PRIMARY KEY(id_categoria)
+)ENGINE=InnoDb;
+
+INSERT INTO t_categorias VALUE(NULL, 'Construcccion'),(null,'Electricos'),(null,'Pisos');
+
+CREATE TABLE IF NOT EXISTS t_productos(
+    id_producto         int(11) auto_increment  NOT NULL,
+    nombre_producto     varchar(100)            NOT NULL,
+    precio              float(100,2)            NOT NULL,
+    stock               int(100)                NOT NULL,
+    oferta              varchar(50)             NOT NULL,
+    fecha               date                    NOT NULL,
+    imagen              varchar(255),
+    id_categoria        int(11)                 NOT NULL,
+    CONSTRAINT          pk_producto             PRIMARY KEY(id_producto),
+    CONSTRAINT          fk_producto_categoria   FOREIGN KEY(id_categoria) REFERENCES t_categorias(id_categoria)
+)ENGINE=InnoDb;
