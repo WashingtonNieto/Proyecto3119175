0e92da25af670e3b1b0343d2fc7c9c238d6b6db5
washington <washington_nieto@hotmail.com>
2025-10-20 20:52:56 -0500

    controlador frontal

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
     class Database{
         public static function connect(){
             //conexion local
-            $db = new msqli('localhost','root','','tienda2');
+            $db = new mysqli('localhost','root','','tienda2');
             $db->query("SET NAMES 'utf8'");
             return $db;
         }
