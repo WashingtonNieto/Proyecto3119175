6880b2f6b1a8e44157cce1000adec86c1f753a0c
washington <washington_nieto@hotmail.com>
2025-10-27 20:49:05 -0500

    error producto controller

--- controllers/productoController.php
+++ controllers/productoController.php
@@ -1,11 +1,15 @@
 <?php
 
-// require_once 'models/producto.php';
+require_once 'models/productoModels.php';
 
 class productoController{
     public function index(){
         $producto = new Producto();
-        $productos = $producto->getRandom(6);
+        $productos = $producto->getAll(6);
+
+        //var_dump($productos->fetch_object());
+//        var_dump($productos->num_rows);
+        //die();
 
         require_once 'views/producto/destacados.php';
     }
